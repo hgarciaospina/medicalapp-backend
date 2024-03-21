e5d51927e2aae9cd7035fc8e964102f4db830dac
Henry García <henrygarciaospina@gmail.com>
2024-03-21 11:49:25 -0500

    Initial structure of the application.

---
+++ src/main/java/com/hegaro/medicalapp/MedicalappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hegaro.medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hegaro/medicalapp/MedicalappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hegaro.medicalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
