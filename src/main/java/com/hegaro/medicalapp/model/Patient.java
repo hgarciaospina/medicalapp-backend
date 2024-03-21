0815e008edca0df290f21e7a76cf8ecd11d82b4f
Henry García <henrygarciaospina@gmail.com>
2024-03-21 12:34:49 -0500

    Definition of necessary annotations in the Patient entity for persistence management.

--- src/main/java/com/hegaro/medicalapp/model/Patient.java
+++ src/main/java/com/hegaro/medicalapp/model/Patient.java
@@ -1,6 +1,14 @@
 package com.hegaro.medicalapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer patientId;
     private String firstName;
     private String lastName;
