7e546e7ce22418e1fbc41b6b233a3d0449c251cd
Henry García <henrygarciaospina@gmail.com>
2024-03-21 19:09:30 -0500

    Restrictions are defined on attributes of the Patient entity.

--- src/main/java/com/hegaro/medicalapp/model/Patient.java
+++ src/main/java/com/hegaro/medicalapp/model/Patient.java
@@ -1,19 +1,21 @@
 package com.hegaro.medicalapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, length = 100)
     private String firstName;
+    @Column(nullable = false, length = 100)
     private String lastName;
+    @Column(nullable = true, length = 300)
     private String address;
+    @Column(nullable = false, length = 10)
     private String phoneNumber;
+    @Column(nullable = false, length = 100)
     private String email;
 
     public Integer getId() {
