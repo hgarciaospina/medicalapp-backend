eb840d6260dacdaafe444ecfc684c24dff7669f3
Henry García <henrygarciaospina@gmail.com>
2024-03-21 19:02:13 -0500

    Definition of the entity Doctor.

---
+++ src/main/java/com/hegaro/medicalapp/model/Doctor.java
@@ -0,0 +1,48 @@
+package com.hegaro.medicalapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 100)
+    private String firstName;
+    @Column(nullable = false, length = 100)
+    private String lastName;
+    @Column(nullable = false, length = 13)
+    private String professionalCard;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfessionalCard() {
+        return professionalCard;
+    }
+
+    public void setProfessionalCard(String professionalCard) {
+        this.professionalCard = professionalCard;
+    }
+}
