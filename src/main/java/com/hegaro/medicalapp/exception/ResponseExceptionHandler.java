0d94bd8e4e789467e3366aaecbc05e8afc46b637
Henry García <henrygarciaospina@gmail.com>
2024-03-22 16:19:08 -0500

    BAD_REQUEST error handling implementation.

--- src/main/java/com/hegaro/medicalapp/exception/ResponseExceptionHandler.java
+++ src/main/java/com/hegaro/medicalapp/exception/ResponseExceptionHandler.java
@@ -1,17 +1,38 @@
 package com.hegaro.medicalapp.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 @ControllerAdvice
 @RestController
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+              HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var errors = ex.getBindingResult().getAllErrors();
+        List<String> errorsMessage = new ArrayList<>();
+        errors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .forEach(errorsMessage::add);
+
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "Validación fállida.", errorsMessage.toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> modelExceptionHandler
             (Exception ex, WebRequest request){
