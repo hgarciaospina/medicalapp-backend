7905f548adac97d5c69d647c76d7c5b2c37bb7d9
Henry García <henrygarciaospina@gmail.com>
2024-03-27 10:53:47 -0500

    Definition of the repository for Consultation.

---
+++ src/main/java/com/hegaro/medicalapp/repository/ConsultationRepository.java
@@ -0,0 +1,7 @@
+package com.hegaro.medicalapp.repository;
+
+import com.hegaro.medicalapp.model.Consultation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultationRepository extends JpaRepository<Consultation, Integer> {
+}
