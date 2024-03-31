c9afa34b5ccef790288592aea2d578b0ad3840ef
Henry García <henrygarciaospina@gmail.com>
2024-03-31 11:38:15 -0500

    Refactorization repository Specialty

--- src/main/java/com/hegaro/medicalapp/repository/SpecialtyRepository.java
+++ src/main/java/com/hegaro/medicalapp/repository/SpecialtyRepository.java
@@ -3,5 +3,5 @@ package com.hegaro.medicalapp.repository;
 import com.hegaro.medicalapp.model.Specialty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpecialtyRepository extends JpaRepository<Specialty, Integer> {
+public interface SpecialtyRepository extends JpaRepository<Specialty, Long> {
 }
