1750c7baf38bc8c5502a7210a3e43857f0bd3a72
Henry García <henrygarciaospina@gmail.com>
2024-03-31 11:25:58 -0500

    Definition of dtos for Specialty services.

---
+++ src/main/java/com/hegaro/medicalapp/controller/dto/request/SpecialtyRequest.java
@@ -0,0 +1,29 @@
+package com.hegaro.medicalapp.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class SpecialtyRequest {
+    @NotNull(message = "Debe ingresar el nombre de la especialidad.")
+    @NotEmpty(message = "El nombre de la especialidad no debe(n) estar vacía .")
+    @NotBlank(message = "El nombre  de la especialidad no debe ir en blanco")
+    @Size(max = 100, message = "El nombre de la especialidad no debe exceder los 100 caracteres.")
+    String name;
+
+    public SpecialtyRequest() {
+    }
+
+    public SpecialtyRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
