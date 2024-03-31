8193bb89c949fe38372c20f6708231a26e46b733
Henry García <henrygarciaospina@gmail.com>
2024-03-31 11:35:25 -0500

    Refactorization model Specialty

--- src/main/java/com/hegaro/medicalapp/model/Specialty.java
+++ src/main/java/com/hegaro/medicalapp/model/Specialty.java
@@ -1,28 +1,27 @@
 package com.hegaro.medicalapp.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 @Entity
 public class Specialty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @NotNull(message = "Debe ingresar el nombre de la especialidad.")
-    @NotEmpty(message = "El nombre de la especialidad no debe(n) estar vacía .")
-    @NotBlank(message = "El nombre  de la especialidad no debe ir en blanco")
-    @Size(max = 100, message = "El nombre de la especialidad no debe exceder los 100 caracteres.")
+    private Long id;
     @Column(nullable = false, length = 100)
     private String name;
 
-    public Integer getId() {
+    public Specialty() {
+    }
+
+    public Specialty(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
