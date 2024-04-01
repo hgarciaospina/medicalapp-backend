820358d3153aa3e3aec535453fb76165d3278595
Henry García <henrygarciaospina@gmail.com>
2024-03-31 23:55:10 -0500

    Refactorization model Exam

--- src/main/java/com/hegaro/medicalapp/model/Exam.java
+++ src/main/java/com/hegaro/medicalapp/model/Exam.java
@@ -1,34 +1,30 @@
 package com.hegaro.medicalapp.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 @Entity
 public class Exam {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @NotNull(message = "Debe ingresar el nombre del examen.")
-    @NotEmpty(message = "El nombre del examen no debe estar vacío.")
-    @NotBlank(message = "El nombre  del examen no debe ir en blanco")
-    @Size(max = 100, message = "El nombre del examen no debe exceder los 100 caracteres.")
+    private Long id;
     @Column(nullable = false, length = 100)
     private String name;
-    @NotNull(message = "Debe ingresar la descripción del examen.")
-    @NotEmpty(message = "La descripción del examen no debe estar vacía.")
-    @NotBlank(message = "La descripción del examen no debe ir en blanco")
-    @Size(max = 300, message = "La descripción del examen debe no debe exceder los 300 caracteres.")
     @Column(nullable = false, length = 300)
     private String description;
 
-    public Integer getId() {
+    public Exam() {
+    }
+
+    public Exam(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
