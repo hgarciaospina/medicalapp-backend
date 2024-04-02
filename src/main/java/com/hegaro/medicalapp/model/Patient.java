2c18ee9413717e7c9d12da373f221e705bf19e72
Henry García <henrygarciaospina@gmail.com>
2024-04-02 03:09:46 -0500

    Refactorization model Patient

--- src/main/java/com/hegaro/medicalapp/model/Patient.java
+++ src/main/java/com/hegaro/medicalapp/model/Patient.java
@@ -1,44 +1,22 @@
 package com.hegaro.medicalapp.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull(message = "Debe ingresar el nombre(s) del paciente.")
-    @NotEmpty(message = "El nombre(s) del paciente no debe(n) estar vacío(s).")
-    @NotBlank(message = "El nombre(s) del paciente no debe ir en blanco")
-    @Size(max = 100, message = "El nombre(s) del paciente debe tener máximo 100 caracteres.")
-    @Size(min = 3, message = "El nombre(s) del paciente debe tener mínimo 3 caracteres.")
     @Column(nullable = false, length = 100)
     private String firstName;
-    @NotNull(message = "Debe ingresar el apellido(s) del paciente.")
-    @NotEmpty(message = "El apellido(s) del paciente no debe(n) estar vacío(s).")
-    @NotBlank(message = "El apellido(s) del paciente no debe ir en blanco")
-    @Size(max = 100, message = "El apellido(s) del páciente debe tener máximo 100 caracteres.")
-    @Size(min = 3, message = "El apellido(s) del páciente debe tener mínimo 3 caracteres.")
     @Column(nullable = false, length = 100)
     private String lastName;
-    @Size(max = 10, message = "El número de documento de identidad debe tener máximo 10 digitos.")
-    @Size(min = 2, message = "El número de documento de identidad debe tener mínimo 2 digitos.")
     @Column(nullable = false, length = 10)
     private String  documentNumber;
-    @Size(max = 300, message = "La dirección debe tener máximo 300 caracteres")
-    @Size(min = 10, message = "La dirección debe tener mínimo 10 caracteres")
     @Column(nullable = false, length = 300)
     private String address;
-    @Size(min=10, max = 10, message = "El número de dispositivo móvil debe tener 10 digitos.")
     @Column(nullable = false, length = 10)
     private String phoneNumber;
-    @Email
-    @Size(max = 100, message = "El correo electrónico debe tener máximo 100 caracteres")
     @Column(nullable = false, length = 100)
     private String email;
 
