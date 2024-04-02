144c8481e7389524b5c15fc65a212a5cae6c16f6
Henry García <henrygarciaospina@gmail.com>
2024-04-01 20:46:18 -0500

    Refactorization repository Patient

--- src/main/java/com/hegaro/medicalapp/repository/PatientRepository.java
+++ src/main/java/com/hegaro/medicalapp/repository/PatientRepository.java
@@ -1,8 +1,9 @@
 package com.hegaro.medicalapp.repository;
 
+import com.hegaro.medicalapp.controller.dto.response.PatientResponse;
 import com.hegaro.medicalapp.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-    Patient findByDocumentNumber(String documentNumber);
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    PatientResponse findByDocumentNumber(String documentNumber);
 }
