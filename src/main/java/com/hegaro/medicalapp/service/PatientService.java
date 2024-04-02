67fc92f08330c29bcff2f93b9a0ac92e1c05ea78
Henry García <henrygarciaospina@gmail.com>
2024-04-01 20:42:58 -0500

    Refactorization service Patient

--- src/main/java/com/hegaro/medicalapp/service/PatientService.java
+++ src/main/java/com/hegaro/medicalapp/service/PatientService.java
@@ -1,7 +1,18 @@
 package com.hegaro.medicalapp.service;
 
+import com.hegaro.medicalapp.controller.dto.request.ExamRequest;
+import com.hegaro.medicalapp.controller.dto.request.PatientRequest;
+import com.hegaro.medicalapp.controller.dto.response.ExamResponse;
+import com.hegaro.medicalapp.controller.dto.response.PatientResponse;
 import com.hegaro.medicalapp.model.Patient;
 
-public interface PatientService extends CrudGeneric<Patient> {
-    Patient findByDocumentNumber(String documentNumber);
+import java.util.List;
+
+public interface PatientService {
+    PatientResponse findByDocumentNumber(String documentNumber);
+    PatientResponse register(PatientRequest examRequest);
+    PatientResponse update(Long id, PatientRequest patientRequest);
+    List<PatientResponse> findAll();
+    PatientResponse findById(Long id);
+    void delete(Long id);
 }
