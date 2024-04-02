e987bcb84029f46d69df047f68862b7fce967244
Henry García <henrygarciaospina@gmail.com>
2024-04-01 20:16:30 -0500

    Definition of dtos for Exam services.

---
+++ src/main/java/com/hegaro/medicalapp/controller/dto/request/ExamRequest.java
@@ -0,0 +1,43 @@
+package com.hegaro.medicalapp.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class ExamRequest {
+    @NotNull(message = "Debe ingresar el nombre del examen.")
+    @NotEmpty(message = "El nombre del examen no debe estar vacío.")
+    @NotBlank(message = "El nombre  del examen no debe ir en blanco")
+    @Size(max = 100, message = "El nombre del examen no debe exceder los 100 caracteres.")
+    String name;
+    @NotNull(message = "Debe ingresar la descripción del examen.")
+    @NotEmpty(message = "La descripción del examen no debe estar vacía.")
+    @NotBlank(message = "La descripción del examen no debe ir en blanco")
+    @Size(max = 300, message = "La descripción del examen debe no debe exceder los 300 caracteres.")
+    private String description;
+
+    public ExamRequest() {
+    }
+
+    public ExamRequest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/hegaro/medicalapp/controller/dto/response/ExamResponse.java
@@ -0,0 +1,40 @@
+package com.hegaro.medicalapp.controller.dto.response;
+
+public class ExamResponse {
+    private Long id;
+    private String name;
+    private String description;
+
+    public ExamResponse() {
+    }
+
+    public ExamResponse(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
