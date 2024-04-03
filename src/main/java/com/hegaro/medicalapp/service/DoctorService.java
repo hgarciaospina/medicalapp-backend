f136d24a4ee22462709948d18c388137b89dbeca
Henry García <henrygarciaospina@gmail.com>
2024-04-02 19:03:49 -0500

    The method findByProfessionalCard return type is changed from Doctor to Optional<Doctor> in the Doctor service.

--- src/main/java/com/hegaro/medicalapp/service/DoctorService.java
+++ src/main/java/com/hegaro/medicalapp/service/DoctorService.java
@@ -1,9 +1,17 @@
 package com.hegaro.medicalapp.service;
 
+import com.hegaro.medicalapp.controller.dto.request.DoctorRequest;
+import com.hegaro.medicalapp.controller.dto.response.DoctorResponse;
 import com.hegaro.medicalapp.model.Doctor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DoctorService {
     Optional<Doctor> findByProfessionalCard(String professionalCard);
+    DoctorResponse register(DoctorRequest doctorRequest);
+    DoctorResponse update(Long id, DoctorRequest doctorRequest);
+    List<DoctorResponse> findAll();
+    DoctorResponse findById(Long id);
+    void delete(Long id);
 }
